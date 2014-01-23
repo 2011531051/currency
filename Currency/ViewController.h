fee4d89b4169783b7ed0e8dcc56299ecb972eb84
Noritsune Riko <solico-66@i.softbank.jp>
2014-01-23 14:34:55 +0900

    円→ドルの変換

--- Currency/ViewController.h
+++ Currency/ViewController.h
@@ -8,6 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    int currentNumber, result;
+}
+
+- (IBAction)Number0:(id)sender;
+- (IBAction)Number00:(id)sender;
+- (IBAction)Number1:(id)sender;
+- (IBAction)Number2:(id)sender;
+- (IBAction)Number3:(id)sender;
+- (IBAction)Number4:(id)sender;
+- (IBAction)Number5:(id)sender;
+- (IBAction)Number6:(id)sender;
+- (IBAction)Number7:(id)sender;
+- (IBAction)Number8:(id)sender;
+- (IBAction)Number9:(id)sender;
+- (IBAction)AllClear:(id)sender;
+- (IBAction)Clear:(id)sender;
+- (IBAction)Convert:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOutput;
+@property (weak, nonatomic) IBOutlet UILabel *resultOutput;
 
 @end
--- Currency/ViewController.m
+++ Currency/ViewController.m
@@ -26,4 +26,85 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Number0:(id)sender {
+    currentNumber = currentNumber * 10;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number00:(id)sender {
+    currentNumber = currentNumber * 100;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number1:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 1;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number2:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 2;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number3:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 3;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number4:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 4;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number5:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 5;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number6:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 6;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number7:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 7;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number8:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 8;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)Number9:(id)sender {
+    currentNumber = currentNumber * 10;
+    currentNumber = currentNumber + 9;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+}
+
+- (IBAction)AllClear:(id)sender {
+    currentNumber = 0;
+    result = 0;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+    _resultOutput.text = [NSString stringWithFormat:@"%d",result];
+}
+
+- (IBAction)Clear:(id)sender {
+
+}
+
+- (IBAction)Convert:(id)sender {
+    result = currentNumber * 0.009535;
+    _numberOutput.text = [NSString stringWithFormat:@"%d",currentNumber];
+    _resultOutput.text = [NSString stringWithFormat:@"%d",result];
+}
+
 @end
